dd2c1ef4acd4303ce851517fd15b8409af8899f0
Kaisei Baba <1935067t@gsuite.kobe-u.ac.jp>
2024-09-03 16:17:19 +0900

    remove getWindowProperty()

--- main.cpp
+++ main.cpp
@@ -437,7 +437,7 @@ int main(int argc, char **argv) {
         }
         char keyC = (char)keyI;
         //escape(27)が押されたとき
-        if(keyI == 27 || cv::getWindowProperty("Main", 1) == -1){
+        if(keyI == 27){
             break;
         }
 
